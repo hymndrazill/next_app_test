3087c54a216b76d29f568b945203fd014dd5db63
hymndrazill <srihafiras@yahoo.fr>
2023-06-14 20:28:34 +0100

    making the password kinda secured

--- src/schemas/index.js
+++ src/schemas/index.js
@@ -2,11 +2,9 @@ import * as yup from "yup"
 
 export const formSchema = yup.object().shape({
     email: yup.string().email("Please provide a valid email").required("Required"),
-    password: yup.string().required("Required")
+    password: yup.string()
+    .min(5, 'Password must be 8 characters long')
+    .matches(/[0-9]/, 'Password requires a number')
+    .matches(/[a-z]/, 'Password requires a lowercase letter')
+    .matches(/[A-Z]/, 'Password requires an uppercase letter').required("Required")
 })
-   
-// .min(5, 'Password must be 8 characters long')
-// .matches(/[0-9]/, 'Password requires a number')
-// .matches(/[a-z]/, 'Password requires a lowercase letter')
-// .matches(/[A-Z]/, 'Password requires an uppercase letter').required("Required")
-// })
