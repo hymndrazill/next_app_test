e34f8b2478e03f64df25123027faf55a38237dbe
hymndrazill <srihafiras@yahoo.fr>
2023-06-14 19:05:17 +0100

    adding session to make sure the user is authed

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -1,23 +1,34 @@
 'use client'
-import { useSession, signIn, signOut } from "next-auth/react"
+
+import {  signIn, signOut, useSession } from "next-auth/react"
+import Link from "next/link"
+import { useRouter } from "next/navigation"
 
 const Dashboard = () => {
-    const session = useSession()
-  if (session.status === 'authenticated') {
-    return (
-      <>
-          Signed in as {session.user.username} <br />
-        <button onClick={() => signOut()}>Sign out</button>
-        
-      </>
-    )
+  const { data: session, status } = useSession()
+  const router = useRouter();
+  const ssession = useSession();
+
+  if (status === "loading") {
+    
+  return "Loading or not authenticated..."
   }
-  return (
-    <>
-      Not signed in <br />
-      <button onClick={() => signIn()}>Sign in</button>
-    </>
-  )
+  if (status === "unauthenticated") {
+    
+  router?.push('/')
+
+  }
+
+    if (status === "authenticated") {
+    console.log(ssession)
+  return <>
+  <h1>Dashboard</h1>
+    <p>Youll only see this if youre authenticated</p>
+    <p>Signed in as {session.user.email}</p>
+    <button onClick={signOut}>LogOut</button>
+  </>
+
+  } 
 }
 
 export default Dashboard
