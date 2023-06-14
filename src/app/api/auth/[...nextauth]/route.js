7c6b7e4d9eaefcf3a4633c0bd254d0715a6998f5
hymndrazill <srihafiras@yahoo.fr>
2023-06-14 19:04:42 +0100

    adding the auth + bcrypt to hash to password

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -2,6 +2,7 @@ import NextAuth from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
 import User from "@/models/User";
 import connect from "@/utilities/db";
+import bcrypt from "bcryptjs";
 
 const handler = NextAuth({
     providers: [
@@ -10,27 +11,34 @@ const handler = NextAuth({
         name: "Credentials",
         async authorize(credentials) {
           await connect();
-  
-          try {
-            const user = await User.findOne({
-              email: credentials.email,
-            });
-              
-            if (user) { 
-                return user
-                }
-             else {
-                throw new Error("User doesn't exist.")
-            }    
-          } catch (err) {
-            throw new Error(err);
-          }
-        },
+            try {
+              const user = await User.findOne({
+                email: credentials.email,
+              });
+                
+              if (user) {
+                const isPasswordCorrect = await bcrypt.compare(
+                  credentials.password,
+                  user.password
+                );
+                  if (isPasswordCorrect) {
+                    console.log(user)
+                    return user;
+                  } else {
+                    throw new Error("Wrong Credentials!");
+                  }
+              } else {
+                throw new Error("User not found!");
+              }
+            } catch (err) {
+              throw new Error(err);
+            }
+          },
       }),
       
     ],
     pages: {
-      error: "/",
+      error: "/dashboard",
     },
   
   });
