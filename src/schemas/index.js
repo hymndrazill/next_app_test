251840d9355d6d79cfc403f96726dd3a6d3ad24f
hymndrazill <srihafiras@yahoo.fr>
2023-06-16 18:47:57 +0100

    adding some regex for the form

--- src/schemas/index.js
+++ src/schemas/index.js
@@ -3,8 +3,8 @@ import * as yup from "yup"
 export const formSchema = yup.object().shape({
     email: yup.string().email("Please provide a valid email").required("Required"),
     password: yup.string()
-    .min(2, 'Password must be 8 characters long')
-    // .matches(/[0-9]/, 'Password requires a number')
-    // .matches(/[a-z]/, 'Password requires a lowercase letter')
-    // .matches(/[A-Z]/, 'Password requires an uppercase letter').required("Required")
+    .min(8, 'Password must be 8 characters long')
+    .matches(/[0-9]/, 'Password requires a number')
+    .matches(/[a-z]/, 'Password requires a lowercase letter')
+    .matches(/[A-Z]/, 'Password requires an uppercase letter').required("Required")
 })
